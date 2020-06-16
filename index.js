4683ef16261e855d30775cd4d8aaabd5ea924be3
Cody Frank <cody.frank30@gmail.com>
2020-06-16 12:35:39 -0400

    create and wire api/users/register route

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
+// import routes
+const authRoute = require('./routes/auth')
 
+// Routes middlewares
+app.use('/api/users', authRoute())
 
 
 
